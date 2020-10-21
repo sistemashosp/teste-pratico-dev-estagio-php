13d96ac07673dc8b7718bbcb15729372c2911d8e
Pedro <pedrogouveadoamaral@gmail.com>
2020-10-21 11:57:51 -0300

    Alterção feita por Pedro Gouvêa do Amaral

--- lista.php
+++ lista.php
@@ -1,1 +1,33 @@
-<?php echo "Seu código aqui"; ?>
+<?php
+
+$handle = fopen("pacientes.csv", "r");
+$row = 0;
+
+while ($linha = fgetcsv($handle, 1000, ",")) {
+	if ($row++ == 0) {
+		continue;
+	}
+	
+	$paciente[] = [
+		'nome' -> $linha[0],
+		'sobrenome' -> $linha[1],
+		'email' -> $linha[2],
+        'datanascimento' -> $linha[3],
+        'genero' -> $linha[4],
+        'tiposanguineo' -> $linha[5],
+        'endereco' -> $linha[6],
+        'cidade' -> $linha[7],
+        'estado' -> $linha[8],
+        'cep' -> $linha[9],
+        'cpf' -> $linha[10],
+        'frkPlanoSaude' -> $linha[11]
+	];
+}
+
+//echo '<pre>';
+print_r($paciente);
+
+fclose($handle);
+
+
+?>
