c9f0487f230d6bc0241b78c1742c35480bb67fd8
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-10-21 23:39:05 -0300

    itera pelas linhas

--- cadastra.php
+++ cadastra.php
@@ -1,10 +1,18 @@
 <?php
 
 // your code here
-$cadastrosCsv = fopen('pacientes.csv','r');
-$listinha = array();   
+function storeAllPatients(){
+    $cadastrosCsv = fopen('pacientes.csv','r');
+    $listinha = array();   
 
-    $cabecalho = fgetcsv($cadastrosCsv,1000,',');    
-    $dados = fgetcsv($cadastrosCsv,1000,',');
+        $cabecalho = fgetcsv($cadastrosCsv,1000,',');    
+        $dados = fgetcsv($cadastrosCsv,1000,',');
+        $listinha[] = array_combine($cabecalho,$cabecalho);
+        while($dados){      
+            $listinha[] = array_combine($cabecalho,$dados);    
+            $dados = fgetcsv($cadastrosCsv,1000,',');      
+        break;  
+        }
 
+}
 ?>
