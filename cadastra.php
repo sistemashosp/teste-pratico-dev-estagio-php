990db3ac9d9a24e3062f0892560ae57531a56a58
ecrseer <juustino@live.com>
2020-10-25 11:54:53 -0300

    mvc table

--- cadastra.php
+++ cadastra.php
@@ -1,73 +1,83 @@
 <?php
 
 // your code here
-function storeAllPatients(){
-    $cadastrosCsv = fopen('pacientes.csv','r');
-    $listinha = array();   
+function cadastra(){
+  global $paginac;
+  $paginac=0;
 
-        $cabecalho = fgetcsv($cadastrosCsv,1000,',');    
-        $dados = fgetcsv($cadastrosCsv,1000,',');
-        $listinha[] = array_combine($cabecalho,$cabecalho);
-    
-        $i=0;
-        echo "<table>";
-        while(($dados)&&($i<5)){      
-            
-            $listinha[] = array_combine($cabecalho,$dados);    
-            $dados = fgetcsv($cadastrosCsv,1000,',');     
-            
-                echo "<tr >";
-                 
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['nome'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['sobrenome'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-
-                      echo $listinha[$i]['cpf'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['email'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['datanascimento'];
-
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['genero'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['tiposanguineo'];
-                    echo "</td>";
-
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['endereco'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['cidade'];
-                    echo "</td>";
-
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['estado'];
-                    echo "</td>";
-                    echo "<td style='padding:0.2em;'>";
-                      echo $listinha[$i]['cep'];
-                    echo "</td>";
+  function showTablePacients($listinha,$i){
+    echo "<tr >";
                   
-                echo "</tr>";
-            
-            $i++;
-       
-        
-        }
-        echo "</table>";
-        
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['nome'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['sobrenome'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
 
-}
+                        echo $listinha[$i]['cpf'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['email'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['datanascimento'];
 
-function storedQuantity($listinha=array()){
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['genero'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['tiposanguineo'];
+                      echo "</td>";
+
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['endereco'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['cidade'];
+                      echo "</td>";
+
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['estado'];
+                      echo "</td>";
+                      echo "<td style='padding:0.2em;'>";
+                        echo $listinha[$i]['cep'];
+                      echo "</td>";
+                    
+                  echo "</tr>";
+  }
+
+  function storeAllPatients(){
+      $cadastrosCsv = fopen('pacientes.csv','r');
+      $listinha = array();   
+
+          $cabecalho = fgetcsv($cadastrosCsv,1000,',');    
+          $dados = fgetcsv($cadastrosCsv,1000,',');
+          $listinha[] = array_combine($cabecalho,$cabecalho);
+      
+          
+          echo "<table>";
+          $paginac=$GLOBALS['paginac'];
+          while(($dados)&&($paginac<5)){      
+              
+              $listinha[] = array_combine($cabecalho,$dados);    
+              $dados = fgetcsv($cadastrosCsv,1000,','); 
+              showTablePacients($listinha,$paginac);
+              $paginac++;
+        
+          }
+          echo "</table>";
+          
+
+  }
+    storeAllPatients();
+    }
+  function insertJavascript(){}  
+    
+
+  function storedQuantity($listinha=array()){
     $i=0;
         foreach($listinha as &$dado){
             $i=$i+1;        
@@ -75,6 +85,5 @@ function storedQuantity($listinha=array()){
         echo "<br/>você tem $i cadasdtros";
 
     }
-  storeAllPatients();
-
+    cadastra();
 ?>
